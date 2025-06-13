20f646df60c750e1fa4a2fabc3e70cda133b7222
IsaacPaez <isaacpaz0123@gmail.com>
2025-06-13 15:20:34 -0500

    Agregar Inventario

--- app/(protected)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function ProtectedIndexPage() {
-  // Redirigir a /dashboard es la solución más común para la ruta índice protegida
-  redirect('/dashboard')
-}
